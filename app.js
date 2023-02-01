7f775bbccb6b24395dca108a3d15afe807e34e56
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-01 14:47:59 -0300

    feat: add contact to table feature

---
+++ app.js
@@ -0,0 +1,41 @@
+const form = document.querySelector(".add-contact_form");
+const nameInput = document.getElementById("input-name")
+const numberInput = document.getElementById("input-number")
+const emailInput = document.getElementById("input-email")
+const contactsTable = document.querySelector("tbody");
+
+console.log(contactsTable)
+console.log(form)
+
+const contatos = [];
+
+
+const addContact = function(nameValue, numberValue, emailValue){
+  contatos.push({nome:nameValue, telefone:numberValue, email:emailValue});
+}
+
+const updateContactTable = function(nameValue,numberValue,emailValue){
+
+const tableRowTemplate = ` 
+<tr>
+<td class="contact-main-info">
+    <div class="contact-image-container">
+        <div class="contact-image">${nameValue.slice(0,1).toUpperCase()+nameValue.slice(1,2).toLowerCase()}</div>
+    </div>
+    <div class="contact-name">${nameValue}</div>
+</td>
+<td>${numberValue}</td>
+<td>${emailValue}</td>
+</tr>`
+
+
+
+contactsTable.insertAdjacentHTML("beforeend", tableRowTemplate)
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addContact(nameInput.value, numberInput.value, emailInput.value);
+  updateContactTable(nameInput.value, numberInput.value, emailInput.value);
+  console.log(contatos);
+});
